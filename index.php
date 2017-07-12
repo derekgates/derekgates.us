250442c91d6bf3ba67eca14f8140f4159b969a84
Derek Gates <derek@derekgates.us>
2017-07-12 13:28:27 -0500

    First commit.  After leaving ActiGraph

---
+++ index.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Derek Gates</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta http-equiv="Content-Language" content="en">
+
+        <meta name="Keywords" content="Derek,Gates,developer,web,contract,asp.net,c#,.net,php,wordpress,html,css,javascript"/>
+
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
+        <link rel="stylesheet" href="css/main.css"/>
+      </head>
+      <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+        <![endif]-->
+          <div class="main-container">
+              <div class="main wrapper clearfix">
+                <h1>Hi there, I am Derek Gates.</h1>
+                <hr/>
+
+                <div class="me right"></div>
+
+                <p>I am a developer that cares about user experience.  I am fluent in desktop and web technologies.
+                   My current work interests user experience design, web architecture, database administration, DevOps and other server side work.  I pride msyelf with designing systems that work together beautifully.</p>
+
+                <p>Previously, I worked at a Pensacola, FL company called ActiGraph.  I was a user experience designer for our websites, hardware device stack developer, a back-end server architect and DevOps for the rest of our products.  I was a member of hardware and software teams and worked hard to communicate ideas.</p>
+
+                <p>Are you interested in a hard working developer on your team?  <a href="resume.pdf">View My Resume</a></p>
+               
+                <p>I want to hear from you: <strong>der<span class="no">4405s</span>ek<span class="no">34rufhaa</span>@de<span class="no">000</span><span class="yes">rekga</span><span class="no">too</span><span class="yes">tes</span>.us</strong></p>
+
+                <ul>
+                  <li><a href="resume.pdf">View My Resume</a></li>
+                  <li><a href="http://blog.jink.us">Blog <small>(this needs updating!)</small></a></li>
+                </ul>
+                
+                <h3>Social</h3>
+                <ul>
+                  <li><a href="https://twitter.com/absolutelyderek">Twitter</a></li>
+                  <li><a href="http://flickr.com/photos/jink">Flickr</a></li>
+                  <li><a href="https://github.com/derekgates">GitHub</a></li>
+                </ul>
+
+
+
+              <?php /*<p>
+                <a class="twitter-timeline" data-dnt=true href="https://twitter.com/tetheredjink" data-widget-id="276231733161099264">Tweets by @tetheredjink</a>
+                <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+                                </p> */ ?>
+
+                <?php /*<script src="http://widgets.twimg.com/j/2/widget.js"></script>
+                <script>
+                new TWTR.Widget({
+                  version: 2,
+                  type: 'profile',
+                  rpp:10,
+                  interval: 6000,
+                  width: 'auto',
+                  height: 410,
+                  theme: {
+                    shell: {
+                      background: '#fcfcfc',
+                      color: '#1a171a'
+                    },
+                    tweets: {
+                      background: '#ffffff',
+                      color: '#0a090a',
+                      links: '#1975ba'
+                    }
+                  },
+                  features: {
+                    scrollbar: true,
+                    loop: false,
+                    live: true,
+                    hashtags: true,
+                    timestamp: true,
+                    avatars: false,
+                    behavior: 'all'
+                  }
+                }).render().setUser('tetheredjink').start();
+                </script>
+                </p>
+                  <!--<p id="description">
+                  Photography taken with a <em>Canon 40D</em> and a <em>Sigma 30mm f/1.4</em>. I currently reside in Charlotte, North Carolina.
+                  I am a self taught photographer, working hard to become great.
+                  </p>
+                  <p> 
+                For contract work, contact me: <strong>ji<span class="no">34rufhaa</span>nk@wh<span class="no">000</span><span class="yes">o</span>.net</strong>
+                  </p>
+                <div id="flick"><div id="flickr_badge_wrapper"><div id="imgs"><script type="text/javascript" src="http://www.flickr.com/badge_code_v2.gne?show_name=1&count=9&display=latest&size=m&layout=x&source=user&user=25333623%40N08"></script>
+                </div>
+                <div id="flick_source">
+                  <a href="http://www.flickr.com/photos/jink/"><img id="flickr_badge_icon" alt="jink (Derek)'s items" src="http://farm3.static.flickr.com/2127/buddyicons/25333623@N08.jpg?1213131123#25333623@N08" /></a>
+                  <span id="flickr_badge_source_txt"><nobr>Go to</nobr> 
+                  <a href="http://www.flickr.com/photos/jink/">jink (Derek)'s photostream</a></span>
+                  <br clear="all" />
+                </div></div></div>-->*/ ?>
+            </div>
+      </div>
+    <script src="http://static.getclicky.com/88386.js" type="text/javascript"></script>
+    <noscript><p><img alt="Clicky" width="1" height="1" src="http://static.getclicky.com/88386-db11.gif" /></p></noscript>
+    </body>
+</html>
