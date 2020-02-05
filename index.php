af01bff7dc14e537482015a779ed3d84bf508e06
Derek Gates <derek@derekgates.us>
2020-02-05 17:45:16 -0600

    Update description, misc txt, remove old php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <title>Derek Gates</title>
-        <meta name="description" content="I am a developer that is devoted to user experience.  I am fluent in desktop and web technologies. I pride myself with designing systems that work together beautifully.">
+        <meta name="description" content="Developer that is devoted to user experience. I am fluent in desktop and web technologies. I pride myself with designing systems that work together beautifully.">
         <meta name="viewport" content="width=device-width">
         <meta property="og:title" content="Derek Gates: Usability focused developer.">
         <meta property="og:type" content="website">
@@ -13,7 +13,7 @@
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
         <meta http-equiv="Content-Language" content="en">
 
-        <meta name="Keywords" content="Derek,Gates,developer,web,contract,asp.net,c#,.net,php,wordpress,html,css,javascript"/>
+        <meta name="Keywords" content="Derek,Gates,developer,web,contract,asp.net,c#,.net,php,wordpress,html,css,javascript,webpack,babel,esnext"/>
 
 
 		<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=kPvW4AoJ54">
@@ -36,7 +36,7 @@
 
                 <div class="me right"><img src="img/avatar_300x300.jpg?v=kPvW4AoJ54" width="150" height="150" alt="Picture of Me from ~2016"/></div>
 
-                <p>I am a developer that is devoted to user experience.  I am fluent in desktop and web technologies. I pride myself with designing systems that work together beautifully.</p>
+                <p>I am devoted to user experience.  I am fluent in desktop and web technologies. I pride myself with designing systems that work together beautifully.</p>
 
                 <p>I am a front-end developer focusing on Angular and AngularJS.  To power the front-end, I have learned webpack, babel and Typescript.  I have been assisting co-workers with devops help by speeding up builds, documenting and helping formulate guidelines.</p>
                 <p>Previously, I worked at a Pensacola, FL company called ActiGraph which made FDA certified activity monitors.  I was a full stack developer that developed front-end software and websites, interfaced with hardware devices, a back-end server architect and DevOps for the rest of our products.  I was a member of hardware and software teams and worked hard to communicate ideas.</p>
@@ -68,61 +68,6 @@
                   <li><a href="https://twitter.com/absolutelyderek">Twitter</a></li>
                   <li><a href="https://medium.com/@derek_60200/has-recommended">Medium Recommendations</a></li>
                 </ul>
-
-
-
-              <?php /*<p>
-                <a class="twitter-timeline" data-dnt=true href="https://twitter.com/tetheredjink" data-widget-id="276231733161099264">Tweets by @tetheredjink</a>
-                <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-                                </p> */ ?>
-
-                <?php /*<script src="http://widgets.twimg.com/j/2/widget.js"></script>
-                <script>
-                new TWTR.Widget({
-                  version: 2,
-                  type: 'profile',
-                  rpp:10,
-                  interval: 6000,
-                  width: 'auto',
-                  height: 410,
-                  theme: {
-                    shell: {
-                      background: '#fcfcfc',
-                      color: '#1a171a'
-                    },
-                    tweets: {
-                      background: '#ffffff',
-                      color: '#0a090a',
-                      links: '#1975ba'
-                    }
-                  },
-                  features: {
-                    scrollbar: true,
-                    loop: false,
-                    live: true,
-                    hashtags: true,
-                    timestamp: true,
-                    avatars: false,
-                    behavior: 'all'
-                  }
-                }).render().setUser('tetheredjink').start();
-                </script>
-                </p>
-                  <!--<p id="description">
-                  Photography taken with a <em>Canon 40D</em> and a <em>Sigma 30mm f/1.4</em>. I currently reside in Charlotte, North Carolina.
-                  I am a self taught photographer, working hard to become great.
-                  </p>
-                  <p> 
-                For contract work, contact me: <strong>ji<span class="no">34rufhaa</span>nk@wh<span class="no">000</span><span class="yes">o</span>.net</strong>
-                  </p>
-                <div id="flick"><div id="flickr_badge_wrapper"><div id="imgs"><script type="text/javascript" src="http://www.flickr.com/badge_code_v2.gne?show_name=1&count=9&display=latest&size=m&layout=x&source=user&user=25333623%40N08"></script>
-                </div>
-                <div id="flick_source">
-                  <a href="http://www.flickr.com/photos/jink/"><img id="flickr_badge_icon" alt="jink (Derek)'s items" src="http://farm3.static.flickr.com/2127/buddyicons/25333623@N08.jpg?1213131123#25333623@N08" /></a>
-                  <span id="flickr_badge_source_txt"><nobr>Go to</nobr> 
-                  <a href="http://www.flickr.com/photos/jink/">jink (Derek)'s photostream</a></span>
-                  <br clear="all" />
-                </div></div></div>-->*/ ?>
             </div>
       </div>
 <script type="text/javascript">
