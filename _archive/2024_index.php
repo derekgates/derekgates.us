1c9724365197b65b6deaddb817a3291f32ab3b28
Derek Gates <derek@derekgates.us>
2024-06-27 22:48:06 -0500

    Archive previous main page

---
+++ _archive/2024_index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Derek Gates</title>
+        <meta name="description" content="Developer that is devoted to user experience. I am fluent in desktop and web technologies. I pride myself with designing systems that work together beautifully.">
+        <meta name="viewport" content="width=device-width">
+        <meta property="og:title" content="Derek Gates: Usability focused developer.">
+        <meta property="og:type" content="website">
+        <meta property="og:image" content="https://derekgates.us/img/avatar_300x300.jpg?v=kPvW4AoJ54">
+
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta http-equiv="Content-Language" content="en">
+
+        <meta name="Keywords" content="Derek,Gates,developer,web,contract,asp.net,c#,.net,php,wordpress,html,css,javascript,webpack,babel,esnext"/>
+
+
+		<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=kPvW4AoJ54">
+		<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=kPvW4AoJ54">
+		<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=kPvW4AoJ54">
+		<link rel="manifest" href="/site.webmanifest?v=kPvW4AoJ54">
+		<link rel="mask-icon" href="/safari-pinned-tab.svg?v=kPvW4AoJ54" color="#5bbad5">
+		<link rel="shortcut icon" href="/favicon.ico?v=kPvW4AoJ54">
+		<meta name="msapplication-TileColor" content="#da532c">
+		<meta name="theme-color" content="#ffffff">
+
+
+        <link rel="stylesheet" href="css/main.css"/>
+      </head>
+      <body>
+          <div class="main-container">
+              <div class="main wrapper clearfix">
+                <h1>Hi there, I am Derek Gates.</h1>
+                <hr/>
+
+                <div class="me right"><img src="img/avatar_300x300.jpg?v=kPvW4AoJ54" width="150" height="150" alt="Picture of Me from ~2016"/></div>
+
+                <p>I am devoted to user experience.  I am fluent in desktop and web technologies. I pride myself with designing systems that work together beautifully.</p>
+
+                <p>I am a front-end developer focusing on Angular and AngularJS.  To power the front-end, I have learned webpack, babel and Typescript.  I have been assisting co-workers with devops help by speeding up builds, documenting and helping formulate guidelines.</p>
+                <p>Previously, I worked at a Pensacola, FL company called ActiGraph which made FDA certified activity monitors.  I was a full stack developer that developed front-end software and websites, interfaced with hardware devices, a back-end server architect and DevOps for the rest of our products.  I was a member of hardware and software teams and worked hard to communicate ideas.</p>
+
+                <h2>Skills</h2>
+                <p>
+                  <ul>
+                    <li>Accomplished C#,  HTML + CSS, UI developer with 14 years of experience.</li>
+                    <li>Diverse knowledge of ASP.net, Javascript, Databases with 8 years of experience.</li>
+                    <li>I enjoy continuous integration, cloud systems (Azure, AWS, etc) and DevOps with 5 years of experience.</li>
+                    <li>Well versed with working in teams, working remotely, and working independently using Agile work flows.</li>
+                    <li>Toastmasters member.  Not afraid of large audiences and presentations.  I love to talk about and explain systems to other members of a team; capable of talking to all levels from the most technical, to sales team, to marketing, to customers.  Mentorship is a strong point for me.</li>
+                  </ul>
+                </p>
+
+                <p>Are you interested in a hard working, diversely skilled developer on your team?  <a href="resume.pdf">View My Full Resume <small>(pdf)</small></a></p>
+               
+                <p>I want to hear from you: <strong>der<span class="no">4405s</span>ek<span class="no">34rufhaa</span>@de<span class="no">000</span><span class="yes">rekga</span><span class="no">too</span><span class="yes">tes</span>.us</strong></p>
+
+                <ul>
+                  
+                </ul>
+                
+                <h2>Social</h2>
+                <ul>
+                  <li><a href="https://blog.derekgates.us">Blog <small>(this needs updating!)</small></a></li>
+                  <li><a href="https://github.com/derekgates">GitHub</a></li>
+                  <li><a href="https://flickr.com/photos/jink">Flickr</a></li>
+                  <li><a href="https://twitter.com/absolutelyderek">Twitter</a></li>
+                  <li><a href="https://medium.com/@derek_60200/has-recommended">Medium Recommendations</a></li>
+                </ul>
+            </div>
+      </div>
+<script type="text/javascript">
+var clicky_site_ids = clicky_site_ids || [];
+clicky_site_ids.push(88386);
+(function() {
+  var s = document.createElement('script');
+  s.type = 'text/javascript';
+  s.async = true;
+  s.src = '//static.getclicky.com/js';
+  ( document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0] ).appendChild( s );
+})();
+</script>
+<noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/88386ns.gif" /></p></noscript>
+    </body>
+</html>
